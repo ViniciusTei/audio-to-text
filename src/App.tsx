a32ce86110680b222074dc0f629fe01047e29ddb
ViniciusTei <viniciustprates@gmail.com>
2023-08-16 21:08:05 -0300

    removed next

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>React.js + Tailwind CSS</h1>
+
+    </main>
+  )
+}
+
+export default App
