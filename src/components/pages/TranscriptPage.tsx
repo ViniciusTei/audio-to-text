61d3a3679c963e97ee01ad755e3f892760f8faa6
ViniciusTei <viniciustpraets@gmail.com>
2023-08-17 20:07:54 -0300

    feat add transcription page

--- src/components/pages/TranscriptPage.tsx
+++ src/components/pages/TranscriptPage.tsx
@@ -1,36 +1,25 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useReducer, useState } from 'react'
+
+import { UploadFile } from 'components/domain'
+import openai from 'lib/openai'
+import { Loading } from 'components/ui'
+
+const { speechToText } = openai
 
 function TranscriptPage() {
-  const dropRef = useRef<HTMLDivElement>(null)
+  const [text, setText] = useState<string | null>(null)
+  const [loading, toggleLoading] = useReducer(prev => !prev, false)
 
-  useEffect(() => {
-    dropRef.current?.addEventListener('dragover', handleDragOver)
-    dropRef.current?.addEventListener('drop', handleDrop)
-
-    return () => {
-      dropRef.current?.removeEventListener('dragover', handleDragOver)
-      dropRef.current?.removeEventListener('drop', handleDrop)
-    }
-  }, [])
-
-  function handleDragOver(e: DragEvent) {
-    e.preventDefault()
-    e.stopPropagation()
-  }
-
-  function handleDrop(e: DragEvent) {
-    e.preventDefault()
-    e.stopPropagation()
-
-    if (!e.dataTransfer) {
-      return null
-    }
-
-    const { files } = e.dataTransfer
-
-    if (files && files.length > 0) {
-      const file = files[0]
-      console.log(file)
+  async function handleTranscriptFile(file: File) {
+    try {
+      toggleLoading()
+      const transcriptedText = await speechToText(file)
+      console.log(transcriptedText)
+      setText(transcriptedText)
+    } catch (error) {
+      console.log('Error fetching transcripted text', error)  
+    } finally {
+      toggleLoading()
     }
   }
 
@@ -38,31 +27,27 @@ function TranscriptPage() {
     <div className="w-full px-10 text-center">
       <h1 className="font-bold text-2xl my-4">Transcrever áudio</h1>
 
-      <div ref={dropRef} className="h-56 p-10 flex mt-4 items-center justify-center flex-col text-xl text-white border-2 border-dashed border-gray-500 rounded-xl">
+      {!loading && !text && <UploadFile onUpload={handleTranscriptFile} />}
+
+      {loading && (
+        <div className="flex flex-col items-center justify-center gap-4 text-lg">
+          <Loading/>
         
-        <div>
-          <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-200">
-            Buscar no computador
-          </label>
-          <div className="relative mt-2 rounded-md shadow-sm">
-            <input
-              type="file"
-              name="file"
-              id="file"
-              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-200 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
+          Transcrevendo arquivo...
+
+          <div role="status" className="max-w-sm animate-pulse">
+              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+              <span className="sr-only">Transcrevendo arquivo...</span>
           </div>
         </div>
-        
-        <br/>
-         Ou arraste e solte o arquivo aqui
-         <span
-          role='img'
-          aria-label='emoji'
-        >
-          &#128526;
-        </span>
-      </div>
+      )}
+
+      {text && text}
 
       <div className="flex justify-around mt-8">
         <p className="max-w-lg">Transcreva seu áudio em texto. Use a tecnologia para transformar seus áudios em texto de forma rápida e prática. Não importa qual seja a língua ou o sotaque, nós entendemos e transformamos em texto.</p>
