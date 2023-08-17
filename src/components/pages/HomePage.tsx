aa1e87dd4eda000961c0e9857888957ad458bcc9
ViniciusTei <viniciustpraets@gmail.com>
2023-08-17 19:41:21 -0300

    fix removed duplicated header

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,73 +1,10 @@
-import React, { useState } from 'react'
-import { LoginButton } from 'components/domain'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only text-gray-100">Audio Para Texto</span>
-              <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button 
-              type="button" 
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
-              onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-100">Produto</a>
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-100">Como usar?</a>
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-100">Preços</a>
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-100">Sobre</a>
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <LoginButton />
-          </div>
-        </nav>
-        {mobileMenuOpen && (<div className="lg:hidden" role="dialog" aria-modal="true">
-          <div className="fixed inset-0 z-50"></div>
-          <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-slate-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only text-gray-100">Audio para texto</span>
-                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-              </a>
-              <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-200" onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
-                <span className="sr-only">Fechar menu</span>
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-slate-500">Produto</a>
-                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-slate-500">Como usar?</a>
-                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-slate-500">Preços</a>
-                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-slate-500">Sobre</a>
-                </div>
-                <div className="py-6">
-                  <LoginButton />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>)}
-      </header>
-
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
           <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
@@ -79,7 +16,7 @@ function HomePage() {
               Aproveite a tecnologia para transformar seus áudios em texto de forma rápida e prática. Não importa qual seja a língua ou o sotaque, nós entendemos e transformamos em texto.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Comece agora</a>
+              <Link to="audio" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Comece agora</Link>
               <a href="#" className="text-sm font-semibold leading-6 text-gray-100">Saiba mais <span aria-hidden="true">→</span></a>
             </div>
           </div>
