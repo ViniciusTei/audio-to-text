47d9946f9dc89165c839a83eca1d173269673412
ViniciusTei <viniciustpraets@gmail.com>
2023-08-18 07:28:51 -0300

    fix session to use local storage

--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -2,8 +2,14 @@ import { Session } from "@supabase/supabase-js"
 import { useEffect, useState } from "react"
 import supabase from "lib/api"
 
+const SUPABASE_TOKEN = 'sb-nnfvmpgnudspenaftses-auth-token'
+
 function useSession() {
-  const [currSession, setSession] = useState<Session | null>(null)
+  const [currSession, setSession] = useState<Session | null>(
+    localStorage.getItem(SUPABASE_TOKEN) 
+      ? JSON.parse(localStorage.getItem(SUPABASE_TOKEN) as string) 
+      : null
+  )
 
   function handleLogout() {
     supabase.auth.signOut()
