ea545e5bed2826fc3f72370b88080588265990ce
ViniciusTei <viniciustpraets@gmail.com>
2023-08-19 08:04:42 -0300

    fix function

--- supabase/functions/new-user/index.ts
+++ supabase/functions/new-user/index.ts
@@ -1,8 +1,13 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 import Stripe from "https://esm.sh/stripe@11.16";
+import { corsHeaders } from '../../_shared/cors.ts'
 
 serve(async (req: Request) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
   try {
     const { user_id, user_email } = await req.json()
 
@@ -21,31 +26,46 @@ serve(async (req: Request) => {
     const supabaseClient = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
       Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+      { global: { headers: { ...corsHeaders, Authorization: req.headers.get('Authorization')! } } }
     )
 
-
-    const { data: customer } = await supabaseClient.from('Customer').select().eq('user_id', user_id).single()
+    console.log('Running function ', { user_id, user_email })
+    const { data: spCustomerDatas, error } = await supabaseClient.from('Customer').select().eq('user_id', user_id)
+    const customer = spCustomerDatas ?? []
     
-    if (customer && customer.stripe_id) {
-      const stripe_customer = await stripe.customers.retrieve(customer.stripe_id)
+    if (error) {
+      console.error('Error fetching customer', error)
+      throw error
+    }
+    
+    if (customer[0] && customer[0].stripe_id) {
+      console.log('Customer found ', customer[0])
+      const stripe_customer = await stripe.customers.retrieve(customer[0].stripe_id)
+      console.log('Stripe customer retrieved', stripe_customer)
       return new Response(JSON.stringify({ stripe_customer }), {
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', ...corsHeaders, },
         status: 200,
       })
     }
 
+    console.log('User not found, creating customer')
     const stripe_customer = await stripe.customers.create({ email: user_email })
 
-    await supabaseClient.from('Customer').insert({ user_id, stripe_id: stripe_customer.id })
+    const { error: err } = await supabaseClient.from('Customer').insert({ user_id, stripe_id: stripe_customer.id })
 
+    if (err) {
+      throw err
+    }
+
+    console.log('Stripe customer created', stripe_customer)
     return new Response(JSON.stringify({ stripe_customer }), {
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json', ...corsHeaders },
       status: 200,
     })
   } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      headers: { 'Content-Type': 'application/json' },
+    console.error('Error with the function', error)
+    return new Response(JSON.stringify({ error }), {
+      headers: { 'Content-Type': 'application/json', ...corsHeaders },
       status: 400,
     })
   }
