4d239fa185fe366413049837227b2e3b2fa21337
ViniciusTei <viniciustpraets@gmail.com>
2023-08-20 17:14:09 -0300

    fix profile page history

--- app/src/components/pages/ProfilePage.tsx
+++ app/src/components/pages/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { AiFillPlusCircle, AiFillMinusCircle, AiOutlineDownload } from 'react-icons/ai'
+import { saveAs } from 'file-saver'
 import useSession from 'hooks/useSession'
 import supabase from 'lib/api'
 
@@ -8,27 +9,53 @@ type UserMetada = {
   name: string
   email: string
 }
-// todo
-// - [x] Add history of transcriptions
-// - [ ] Add total of transcriptions
-// - [ ] Add money spent
+
+type Transcription = {
+  id: number
+  created_at: string
+  type: string
+  credits: string
+  text_transcript: string | null
+}
+
 function ProfilePage() {
-  const [trasncriptions, setTranscriptions] = useState([] as any[])
+  const [trasncriptions, setTranscriptions] = useState([] as Transcription[])
+  const [credits, setCredits] = useState(0)
+  const [usedCredits, setUsedCredits] = useState(0)
   const { session } = useSession()
   const userData = session?.user?.user_metadata as UserMetada
 
+  function saveTranscription(transcription: Transcription) {
+    const text = transcription.text_transcript as string
+    const blob = new Blob([text], { type: 'text/plain;charset=utf-8' })
+    saveAs(blob, `${transcription.created_at}.txt`)
+  }
+
   useEffect(() => {
     async function getTranscriptions(user: any) {
+      const { data: customer } = await supabase.from('Customer').select('id').eq('user_id', user.id)
       const { data, error } = await supabase
-        .from('Transcriptions')
-        .select('id, created_at, text_transcript')
-        .eq('user_id', user?.id)
+        .from('Transactions')
+        .select('id, created_at, type, Customer (id, credits), Transcriptions (id, text_transcript)')
+        .eq('user_id', (customer as any)[0].id)
 
       if (error) {
         throw error
       }
       
-      setTranscriptions(data) 
+      const transcriptions = (data as any).map((transaction: any) => {
+        return {
+          id: transaction.id,
+          created_at: new Date(transaction.created_at).toLocaleString('pt-BR', { timeZone: 'UTC' }),
+          type: transaction.type,
+          credits: transaction.Customer.credits,
+          text_transcript: transaction.Transcriptions ? transaction.Transcriptions.text_transcript : null
+        }
+      })
+
+      setTranscriptions(transcriptions)
+      setCredits((data as any)[0].Customer.credits)
+      setUsedCredits(transcriptions.filter((t: Transcription) => t.type === 'transcription').length)
     }
 
     if (session) {
@@ -51,8 +78,8 @@ function ProfilePage() {
           <h2 className="title-font font-medium text-lg">{userData.name}</h2>
           <span className="mb-4 text-gray-500">{userData.email}</span>
           <span className="inline-text">
-            <p className="text-gray-500 flex items-center gap-2"> <AiFillPlusCircle/> 0 creditos</p>
-            <p className="text-gray-500 flex items-center gap-2"> <AiFillMinusCircle/> 0 creditos utilizados</p>
+            <p className="text-gray-500 flex items-center gap-2"> <AiFillPlusCircle/> {credits} creditos</p>
+            <p className="text-gray-500 flex items-center gap-2"> <AiFillMinusCircle/> {usedCredits} creditos utilizados</p>
           </span>
         </div>
       </div>
@@ -71,25 +98,22 @@ function ProfilePage() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="px-4 py-3">Transcrição</td>
-              <td className="px-4 py-3">25/12/1997 14:25</td>
-              <td className="px-4 py-3 text-red-500">- 1</td>
-              <td className="px-4 py-3 text-lg text-white">-</td>
-              <td className="w-10 text-center">
-                <span className="inline-flex items-center rounded-md bg-gray-50 mr-4 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
-                  <button><AiOutlineDownload/></button>
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td className="border-t-2 border-gray-800 px-4 py-3">Pagamento</td>
-              <td className="border-t-2 border-gray-800 px-4 py-3">25/12/1997 14:20</td>
-              <td className="border-t-2 border-gray-800 px-4 py-3 text-green-500">+ 10</td>
-              <td className="border-t-2 border-gray-800 px-4 py-3 text-lg text-white">R$1,00</td>
-              <td className="border-t-2 border-gray-800 w-10 text-center">
-              </td>
-            </tr>
+            {trasncriptions.map((t) => (
+              <tr>
+                <td className="border-t-2 border-gray-800 px-4 py-3">{t.type === 'payment' ? 'Pagamento' : 'Transcrição'}</td>
+                <td className="border-t-2 border-gray-800 px-4 py-3">{t.created_at}</td>
+                <td className="border-t-2 border-gray-800 px-4 py-3">{t.type === 'payment' ? '+ 10' : '- 1'}</td>
+                <td className="border-t-2 border-gray-800 px-4 py-3 text-lg text-white">{t.type === 'payment' ? 'R$1,00' : '-'}</td>
+                <td className="border-t-2 border-gray-800 w-10 text-center">
+                  {t.text_transcript && (
+                    <span className="inline-flex items-center rounded-md bg-gray-50 mr-4 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                      <button onClick={() => saveTranscription(t)}><AiOutlineDownload/></button>
+                    </span>
+                  )}
+                </td>
+              </tr>
+            ))}
+
           </tbody>
         </table>
       </div>
