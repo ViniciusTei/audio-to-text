f2e4eb863114ab43c456ef685eb1ca3994f6228a
ViniciusTei <viniciustpraets@gmail.com>
2023-08-20 13:59:16 -0300

    fix pricing

--- app/src/components/pages/HomePage.tsx
+++ app/src/components/pages/HomePage.tsx
@@ -1,13 +1,6 @@
 import React from 'react'
-import { AiFillApi, AiFillDollarCircle, AiFillMoneyCollect, AiFillRead, AiFillWallet, AiOutlineArrowRight } from 'react-icons/ai'
+import { AiFillApi, AiFillDollarCircle, AiFillRead, AiOutlineArrowRight } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
-
-const includedFeatures = [
-  'Áudios ilimitados',
-  'Transcrições ilimitadas',
-  'Suporte para vários idiomas',
-  'Suporte para vários sotaques',
-]
 
 function HomePage() {
 
@@ -43,9 +36,9 @@ function HomePage() {
           </div>
           <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
             <div className="p-8 sm:p-10 lg:flex-auto">
-              <h3 className="text-2xl font-bold tracking-tight text-gray-200">Pacote de creditos</h3>
+              <h3 className="text-2xl font-bold tracking-tight text-gray-200">Pacote de créditos</h3>
               <p className="mt-6 text-base leading-7 text-gray-100">
-                Um pacote de créditos é válido por 1 ano a partir da data da compra. Você pode utilizar os créditos quando quiser, não tem prazo para utilização.
+                Um pacote de créditos é válido por 1 ano a partir da data da compra. Você ganha 1 crédito ao fazer seu primeiro cadastro. Você pode comprar mais créditos quando quiser por apenas R$1,00.
               </p>
               <div className="mt-10 flex items-center gap-x-4">
                 <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">O que é incluso</h4>
@@ -65,7 +58,7 @@ function HomePage() {
             <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
               <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
                 <div className="mx-auto max-w-xs px-8">
-                  <p className="text-base font-semibold text-gray-600">Pague o valor que quiser</p>
+                  <p className="text-base font-semibold text-gray-600">Pague um valor mínimo</p>
                   <p className="mt-6 flex items-baseline justify-center gap-x-2">
                     <span className="text-5xl font-bold tracking-tight text-gray-900">R$1,00</span>
                     <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">BRL</span>
@@ -77,7 +70,7 @@ function HomePage() {
                     Compre créditos
                   </a>
                   <p className="mt-6 text-xs leading-5 text-gray-600">
-                    Comece a partir de <span className="font-semibold">R$1,00</span> por crédito, e já pode começar a utilizar.
+                    Você tem 1 crédito para utilizar ao fazer seu primeiro cadastro. Você pode comprar mais créditos quando quiser.
                   </p>
                 </div>
               </div>
@@ -108,9 +101,6 @@ function HomePage() {
                 <p className="leading-relaxed text-base">
                   Utilizamos modelos de inteligência artificial para transformar seus áudios em texto. Tentando manter a maior fidelidade possível.
                 </p>
-                <a className="mt-3 text-indigo-400 inline-flex items-center">Saiba mais
-                  <AiOutlineArrowRight/>
-                </a>
               </div>
             </div>
             <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
@@ -124,9 +114,6 @@ function HomePage() {
                 <p className="leading-relaxed text-base">
                   Nosso site é simples e fácil de usar. Basta fazer o upload do seu áudio e aguardar o resultado. Você pode fazer o upload de vários áudios e manter um histórico.
                 </p>
-                <a className="mt-3 text-indigo-400 inline-flex items-center">Saiba mais
-                  <AiOutlineArrowRight/>
-                </a>
               </div>
             </div>
             <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
@@ -140,9 +127,6 @@ function HomePage() {
                 <p className="leading-relaxed text-base">
                   Nosso serviço é barato e você só paga pelo que utilizar. Não tem mensalidade, não tem fidelidade, não tem pegadinha.
                 </p>
-                <a className="mt-3 text-indigo-400 inline-flex items-center">Saiba mais
-                  <AiOutlineArrowRight/>
-                </a>
               </div>
             </div>
           </div>
@@ -151,5 +135,12 @@ function HomePage() {
     </main>
   )
 }
+
+const includedFeatures = [
+  '10 créditos para usar',
+  'Créditos válidos por 1 ano',
+  '1 crédito ao fazer o cadastro',
+  'Acesso ao histórico de transcrições',
+]
 
 export default HomePage
