c299c14c5a9eb0f5b689a4124c643d81274de4ef
ViniciusTei <viniciustpraets@gmail.com>
2023-08-21 09:02:39 -0300

    feat add snap

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 import './globals.css'
 
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement!)
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const AppElement = () => ( 
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
 
+if (rootElement?.hasChildNodes()) {
+  hydrateRoot(
+    rootElement,
+    <AppElement />
+  )
+} else {
+  root.render(<AppElement />)
+}
